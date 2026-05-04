fbd6952aabba5786cce0de89530a42270fa865d1
luizhluz <luiz.h.luz@edu.senai.br>
2026-05-04 11:09:47 -0300

    Update Cpf.py

--- Cpf.py
+++ Cpf.py
@@ -1,31 +1,37 @@
-def validar_cpf(cpf):
-    # Remove caracteres não numéricos
-    cpf = ''.join(filter(str.isdigit, cpf))
-
-    # Verifica tamanho e sequência inválida
-    if len(cpf) != 11 or cpf == cpf[0] * 11:
-        return False
-
-    # Cálculo do 1º dígito
-    soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
-    dig1 = 11 - (soma % 11)
-    if dig1 >= 10:
-        dig1 = 0
-
-    # Cálculo do 2º dígito
-    soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
-    dig2 = 11 - (soma % 11)
-    if dig2 >= 10:
-        dig2 = 0
-
-    # Verifica os dígitos
-    return dig1 == int(cpf[9]) and dig2 == int(cpf[10])
-
-
-# Programa principal
 cpf = input("Digite o CPF: ")
 
-if validar_cpf(cpf):
-    print("CPF válido!")
-else:
+
+cpf = cpf.replace(".", "")
+cpf = cpf.replace("-", "")
+
+if len(cpf) != 11:
     print("CPF inválido!")
+else:
+    if cpf == cpf[0] * 11:
+        print("CPF inválido!")
+    else:
+     
+        soma = 0
+        for i in range(9):
+            soma = soma + int(cpf[i]) * (10 - i)
+
+        resto = soma % 11
+        dig1 = 11 - resto
+        if dig1 >= 10:
+            dig1 = 0
+
+       
+        soma = 0
+        for i in range(10):
+            soma = soma + int(cpf[i]) * (11 - i)
+
+        resto = soma % 11
+        dig2 = 11 - resto
+        if dig2 >= 10:
+            dig2 = 0
+
+      
+        if dig1 == int(cpf[9]) and dig2 == int(cpf[10]):
+            print("CPF válido!")
+        else:
+            print("CPF inválido!")
